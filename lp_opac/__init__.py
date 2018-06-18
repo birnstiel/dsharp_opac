ab7736dc628c3b0def02e675dd622749d08ea193
birnstiel@me.com <birnstiel@me.com>
2018-06-18 14:56:11 +0200

    added proper extrapolation for default opacity

--- lp_opac/__init__.py
+++ lp_opac/__init__.py
@@ -4,7 +4,8 @@ from .lp_opac import compare_nk, diel_luca, diel_const, diel_mixed, \
     diel_vacuum, diel_warren, diel_draine2003_astrosil, diel_WD2001_astrosil, \
     diel_zubko_carbon, diel_dl84_astrosil, diel_from_lnk_file, \
     gaussian_N_of_a, get_default_diel_constants, get_default_opacities, \
-    get_opacity_from_distribution, get_total_opacity, powerlaw_N_of_a
+    get_opacity_from_distribution, get_total_opacity, powerlaw_N_of_a, \
+    diel_henning, download
 
 __all__ = [
     'bhmie_python',
@@ -27,4 +28,6 @@ __all__ = [
     'get_opacity_from_distribution',
     'get_total_opacity',
     'powerlaw_N_of_a',
+    'diel_henning',
+    'download'
     ]
