5c04ad127d6e031dd29794488b7819d7ce13e1d6
birnstiel <birnstiel@me.com>
2020-11-11 15:25:59 +0100

    version handling improved; version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 """
 Setup file for package `dsharp_opac`.
 """
-import setuptools # noqa
+import setuptools  # noqa
 from numpy.distutils.core import Extension
 import pathlib
 import warnings
@@ -11,19 +11,30 @@ PACKAGENAME = 'dsharp_opac'
 # the directory where this setup.py resides
 HERE = pathlib.Path(__file__).parent
 
+
+def read_version():
+    with (HERE / PACKAGENAME / '__init__.py').open() as fid:
+        for line in fid:
+            if line.startswith('__version__'):
+                delim = '"' if '"' in line else "'"
+                return line.split(delim)[1]
+        else:
+            raise RuntimeError("Unable to find version string.")
+
+
 if __name__ == "__main__":
     from numpy.distutils.core import setup
 
     extensions = [
         Extension(name='dsharp_opac.bhmie_fortran', sources=['dsharp_opac/bhmie_fortran.f90']),
         Extension(name='dsharp_opac.fit_module', sources=['dsharp_opac/fit_module.f90']),
-        ]
+    ]
 
     def setup_function(extensions):
         setup(
             name=PACKAGENAME,
             description='python routines to calculate mie opacities',
-            version='1.1.4',
+            version=read_version(),
             long_description=(HERE / "README.md").read_text(),
             long_description_content_type='text/markdown',
             url='https://github.com/birnstiel/dsharp_opac',
@@ -36,12 +47,12 @@ if __name__ == "__main__":
                 'optical_constants/*/*.*',
                 'optical_constants/*/*/*.*',
                 'data/*.*',
-                ]},
+            ]},
             include_package_data=True,
             install_requires=['scipy', 'numpy', 'matplotlib', 'astropy', 'mpmath'],
             zip_safe=False,
             ext_modules=extensions
-            )
+        )
     try:
         setup_function(extensions)
     except BaseException:
